11adb9283d4fd132f1ff78695aa5880a56ce72e6
Marah Rice <91274269+MarahDeven@users.noreply.github.com>
2022-02-16 11:59:21 -0500

    created the Weather component after initial index.js page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+import Weather from "./Weather";
 
-import App from "./App";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Weather city="London" temperature={7} />
+    </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
