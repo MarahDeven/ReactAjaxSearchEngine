fbfba9669d8a8d2652dd753e5d0185cb4a37961b
Marah Rice <91274269+MarahDeven@users.noreply.github.com>
2022-02-16 12:44:36 -0500

    finished function and added css

--- src/Search.js
+++ src/Search.js
@@ -1,52 +1,57 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./Search.css";
 
-export default function Search(props) {
-  let weather [
-    let [temperature, setTemperature] = useState(null);
-    let [description, setDescription] = useState(null);
-    let [humidity, setHumidity] = useState(null);
-    let [wind, setWind] = useState(null);
-    let [city, setCity] = useState(null);
-  ]
+export default function Search() {
+  const [city, setCity] = useState(null);
+  const [weather, setWeather] = useState({});
+  const [load, setLoad] = useState("false");
+
+  function showWeather(response) {
+    setLoad(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    });
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
     let apiKey = "4af869c2b16527dd3b375c7f352dada0";
     let units = "imperial";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-    axios.get(url).then(showTemperature);
-    axios.get(url).then(showDescription);
-    axios.get(url).then(showHumidity);
-    axios.get(url).then(showWind);
+    axios.get(url).then(showWeather);
   }
 
-  function showCity(event) {
-    setCity(event.target.value);
-  }
-
-  function showTemperature(event) {
-    setTemperature(response.data.main.temp);
-  }
-
-  function showDescription(event) {
-    setDescription(response.data.weather[0].description);
-  }
-
-  function showHumidity(event) {
-    setHumidity(response.data.main.humidity);
-  }
-
-  function showWind(event) {
-    setWind(response.data.wind.speed);
-  }
-
-  return (
-    <div className="Weather">
-      <form onSubmit={handleSubmit}>
-        <input type="Search" placeholder="Type a city" onChange={showCity} />
-        <input type="submit" value="Search" />
-      </form>
-    </div>
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Type a city" onChange={updateCity} />
+      <input type="submit" value="Search" />
+    </form>
   );
+  if (load) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C </li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Windspeed: {Math.round(weather.wind)} km/h</li>
+          <li>
+            <img src={weather.icon} alt="weather-icon" />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
