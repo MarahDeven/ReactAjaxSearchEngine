e9c39bcb8e56a90c3798187c08a24127362b469a
Marah Rice <91274269+MarahDeven@users.noreply.github.com>
2022-02-17 13:04:11 -0500

    created function to update city with setCity

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -2,15 +2,19 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function WeatherSearch() {
-  let [city, setCity] = useState("");
+  const [city, setCity] = useState("");
 
   function handlSubmit(event) {
     event.preventDefault();
   }
 
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <form onSubmit={handlSubmit}>
-      <input type="search" placeholder="enter a city" />
+      <input type="search" placeholder="enter a city" onChange={updateCity} />
       <input type="submit" value="Search" />
     </form>
   );
