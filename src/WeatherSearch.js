9fcbd045dc31292343d3760650c1a975564e701f
Marah Rice <91274269+MarahDeven@users.noreply.github.com>
2022-02-17 13:14:04 -0500

    created if/else statement for displaying weather

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -3,22 +3,32 @@ import axios from "axios";
 
 export default function WeatherSearch() {
   const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+
+  function displayWeather(response) {
+    setLoaded(true);
+    console.log(response.data.main.temp);
+  }
 
   function handlSubmit(event) {
     event.preventDefault();
     let apiKey = "4af869c2b16527dd3b375c7f352dada0";
     let units = "imperial";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(url).then(displayWeather);
   }
 
   function updateCity(event) {
     setCity(event.target.value);
   }
-
-  return (
-    <form onSubmit={handlSubmit}>
-      <input type="search" placeholder="enter a city" onChange={updateCity} />
-      <input type="submit" value="Search" />
-    </form>
-  );
+  if (loaded) {
+    return "Loaded";
+  } else {
+    return (
+      <form onSubmit={handlSubmit}>
+        <input type="search" placeholder="enter a city" onChange={updateCity} />
+        <input type="submit" value="Search" />
+      </form>
+    );
+  }
 }
